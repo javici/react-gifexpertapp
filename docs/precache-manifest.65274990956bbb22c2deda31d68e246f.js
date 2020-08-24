0c7785a1dd7fe1fc3a9e73623d48fa447c0f4ba7
javici <javcasados@gmail.com>
2020-08-24 17:14:28 +0200

    Docs added

---
+++ docs/precache-manifest.65274990956bbb22c2deda31d68e246f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4055eb1adca9788dad081bb4b438c196",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d3c7b55eb77c63d5a1a",
+    "url": "/static/css/main.703197eb.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d3c7b55eb77c63d5a1a",
+    "url": "/static/js/main.e22419dc.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
